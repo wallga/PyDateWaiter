1daa8d5ec7cab4d3e95e55d5cd0b919451eec119
wallga <93985275+wallga@users.noreply.github.com>
2022-01-16 21:28:05 +0900

    Beta 1.1 version release
    1.decide Anniversary is int or not
    2.Calculate code

--- PyDateWaiter_Beta.py
+++ PyDateWaiter_Beta.py
@@ -3,24 +3,43 @@
 # Build for Python 3.x
 
 import datetime
- 
+
+# Time valuables
 now = datetime.datetime.now()       
-nowY = now.strftime("%Y")
-nowM = now.strftime("%m")
-nowD = now.strftime("%d")
+nowY = now.year
+nowM = now.month
+nowD = now.day
+nowh = now.hour
+nowm = now.minute
+
 
 anniv_name = input("Anniversary Name : ")
 anniv = input("Anniversary Date : ") # 8 
 annivY = anniv[0:4]
 annivM = anniv[4:6]
 annivD = anniv[6:]
+anniv_tuple = (annivY, annivM, annivD, 0, 0, 0)
 
+# Int Error 
+try:
+    int(anniv)
+    annivInt = True
+except ValueError:
+    annivInt = False
+if annivInt == False:
+    print("ValueError : Anniversary date must be number. (ex:080701)")
+    exit()
+
+# Calculate Code
 nthY = int(nowY) - int(annivY)
+diffrenceD = (anniv_tuple - now)
+print(diffrenceD)
 
-if nowY != annivY and nowM == annivM and nowD == annivD:
-    print("Today is {0}th {1}! Congratulations!".format(nthY, anniv_name))
-elif nowY == annivY and nowM == annivM and nowD == annivD:
-    print("Today is {0}!".format(anniv_name))
-else:
-    print("There are {0} days left until the anniversary.") # uncode
-    ###
+# Decide Code
+while True:
+    if nowY != annivY and nowM == annivM and nowD == annivD:
+        print("Today is {0}th {1}! Congratulations!".format(nthY, anniv_name))
+    elif nowY == annivY and nowM == annivM and nowD == annivD:
+        print("Today is {0}!".format(anniv_name))
+    else:
+        print("There are {0} days left until the anniversary.") 
